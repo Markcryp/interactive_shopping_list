7453c1e4aaeeda1a68e3ddc7fe6cc4fd3bb1152c
Mark <mark.njau@student.moringaschool.com>
2024-07-10 16:15:52 +0300

    Add utility functions.  add DOMContentLoaded event listener. Add function to display items. Create list and append it to <0l> tag. Add a function to display items. function to create a list of items.

---
+++ script/app.js
@@ -0,0 +1,43 @@
+// Utility Functions od DOM Manipulation
+function createAnElement(element) {
+    return document.createElement(element);
+}
+
+function addText(element, text){
+    return (element.innerText = text);
+}
+
+function appendChild(child, parent) {
+    return parent.appendChild(child);
+}
+
+function select(selector) {
+    return document.querySelector(selector);
+}
+
+function listen(element, event, callback) {
+ return element.addEventListener(event, callback);
+}
+
+function addAtttribute(element, attribute, content) {
+    return element.setAttribute(attribute, content);
+} 
+
+const shoppingList = ['milk', 'Tea']
+
+
+const ol = select('ol');
+
+listen(document, "DOMContentLoaded", displayItems);
+
+function displayItems() {
+    shoppingList.forEach(createAListItem);
+}
+
+function createAListItem(item) {
+    const li = createAnElement('li');
+    addText(li, item);
+    appendChild(li, ol);
+}
+
+
